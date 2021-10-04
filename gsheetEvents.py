daadb29fc3265bdd0ed5f8f8e2dffdf193d47224
Stephan K <55259393+skruglov2023@users.noreply.github.com>
2021-10-03 23:14:54 -0500

    Delete gsheetEvents.py

--- gsheetEvents.py
+++
@@ -1,5 +0,0 @@
-import gdown
-
-url = 'https://docs.google.com/spreadsheets/d/1ciDwVxGTlD7Gf6jnPWBlJl2DYT9WFJioan3xhFJnfVY/export?format=csv'
-output = 'CDNEvents.txt'
-gdown.download(url, output, quiet=False)
