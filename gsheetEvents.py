86adb9b5e7c84a4b97f25f447df67059950717cc
Stephan K <55259393+skruglov2023@users.noreply.github.com>
2021-10-07 20:38:24 -0500

    Create gsheetEvents.py

---
+++ gsheetEvents.py
@@ -0,0 +1,5 @@
+-import gdown
+-
+-url = 'https://docs.google.com/spreadsheets/d/1ciDwVxGTlD7Gf6jnPWBlJl2DYT9WFJioan3xhFJnfVY/export?format=csv'
+-output = '/home/pi/Desktop/scripts/CDN-Discord-Bot/CDNEvents.txt'
+-gdown.download(url, output, quiet=False)
