a5aa8875d4ed5c0b41b083a6c1eb6b83fc7bb297
skruglov2023 <skruglov.2023@carmelhs.org>
2021-11-09 19:39:09 -0600

    Added clear command

--- CDNEventBot.py
+++ CDNEventBot.py
@@ -7,8 +7,8 @@ path0='/home/pi/Desktop/scripts/CDN-Discord-Bot/gsheetEvents.py'
 path1='/home/pi/Desktop/scripts/CDN-Discord-Bot/CDNEventsCleaner.py'
 path2='/home/pi/Desktop/scripts/CDN-Discord-Bot/CDNEvents.txt'
 path3='/home/pi/Desktop/scripts/CDN-Discord-Bot/blocked_words.txt'
-TokenPath = '/home/pi/Desktop/scripts/token'
-GuildPath='/home/pi/Desktop/scripts/guild'
+TokenPath = '/home/pi/Desktop/scripts/CDN_token'
+GuildPath='/home/pi/Desktop/scripts/CDN_guild'
 
 with open(GuildPath, 'r') as guil:
     global GUILD
@@ -80,6 +80,9 @@ async def on_message(message):
     event_response_channel=client.get_channel(882667880028708874)
     channell=client.get_channel(881007767018700860)
     stephan=client.get_user(675726066018680861)
+    role_change=client.get_channel(881007767018700860)
+    producers=discord.utils.get(id.roles, id=880274405836599356)
+#    print(producers)
     if message.author == client.user:
         return
     msg=message.content
@@ -104,27 +107,47 @@ async def on_message(message):
         await message.channel.send(CDNEvents.read())
         await message.delete()
     elif message.content.startswith('$help'):
-        help_string=f"""Your possible commands are:
-        $hello: I say hello to you
-        $welcome: I tell you what to do if you recently joined and don\'t know what to do
-        $showevents: I tell you what the next events are that CDN has been invited to
-        $showme: Shows your name and profile picture
-        $help: View this message again
-        $givemerole followed by one of the following roles, case-sensitive: Afterhours, Swear-A-Lot, Mass, Senior
-        $ and any characters: I will respond with Invalid Command
-        Any words that don\'t include $: I will ignore your text
-        Autonomous program: When you post something in {event_channel.mention}, I will add 3 reactions, and will list the names of respondents in {event_response_channel.mention}
-        There is a list of words that you can't say. Any messages containing them will be deleted.  
-        All messages, when deleted or edited, will be recorded
-        If you have any questions, please message {stephan.mention}"""
-        embed=discord.Embed(title="CDN Bot Support", description="", color=discord.Colour.from_rgb(234, 170, 0))
-        embed.add_field(name='help page', value=help_string, inline=True)
-        channel=message.channel
-        await channel.send(embed=embed)
+        if "roles" in message.content.lower():
+            roles = '\n - '.join([role.name for role in id.roles])
+#            print(f'Roles:\n - {roles}')
+            help_string=roles
+            embed=discord.Embed(title="Available CDN Discord Roles", description="", color=discord.Colour.from_rgb(234, 170, 0))
+            embed.add_field(name='Use $givemerole with any role above the CDN Events role \nKeep in mind that none of these roles give you anything special \n', value=help_string, inline=True)
+            channel=message.channel
+            await channel.send(embed=embed)
+        else:
+#            print("help")
+            help_string=f"""
+		$hello: I say hello to you
+		$welcome: I tell you what to do if you recently joined and don\'t know what to do
+		$showevents: I tell you what the next events are that CDN has been invited to
+		$showme: Shows your name and profile picture
+		$help: See this message again
+		$givemerole followed by a role that you want or need: You can\'t get mod, so don't bother trying to. If I can't give you a role, an admin will give it to you if necessary
+		$showthem and user_id: Don't know why you need that, but it gives you the person's username and profile picture. You need developer mode enabled to get user_id
+		$ with a non-command: I will respond with Invalid Command
+		Autonomous program: When you post something in {event_channel.mention}, I will add 3 reactions, and will list the names of respondents in {event_response_channel.mention}
+		All messages, when deleted or edited, will be recorded
+		If you have any questions, please message {stephan.mention}"""
+            embed=discord.Embed(title="CDN Bot Support", description="", color=discord.Colour.from_rgb(234, 170, 0))
+            embed.add_field(name='Possible commands include:', value=help_string, inline=True)
+            channel=message.channel
+            await channel.send(embed=embed)
         await message.delete()
     elif message.content.startswith('$showme'):
         embed=discord.Embed(title=f"{message.author}", description="", color=discord.Color.green()) # F-Strings!
         embed.set_thumbnail(url=message.author.avatar_url)
+        await message.channel.send(embed=embed)
+        await message.delete()
+    elif message.content.startswith('$showthem'):
+        show=message.content
+        print(show)
+        show=show[10:]
+        print(show)
+        show_user= await client.fetch_user(show)
+        print(show_user)
+        embed=discord.Embed(title=f"{show_user}", description="", color=discord.Color.blue())
+        embed.set_thumbnail(url=show_user.avatar_url)
         await message.channel.send(embed=embed)
         await message.delete()
     elif 'happy birthday' in message.content.lower():
@@ -134,12 +157,27 @@ async def on_message(message):
 #        print(role_name)
         role_name=role_name[12:]
 #        print(role_name)
-        role = discord.utils.get(id.roles, name=role_name)
-        await message.author.add_roles(role)
-        embed=discord.Embed(title=f"{message.author} requested {role_name}", description="", color=discord.Colour.blue())
-        embed.add_field(name=message.content, value="Role request", inline=True)
-        logChan=client.get_channel(881026004154482709)
-        await logChan.send(embed=embed)
+        if message.channel==role_change:
+            role = discord.utils.get(id.roles, name=role_name)
+            await message.author.add_roles(role)
+            embed=discord.Embed(title=f"{message.author} requested {role_name}", description="", color=discord.Colour.blue())
+            embed.add_field(name=message.content, value="Role request", inline=True)
+            logChan=client.get_channel(881026004154482709)
+            await logChan.send(embed=embed)
+        else:
+            await message.channel.send(f"Roles can't be requested here. Please use {role_change.mention}", delete_after=5)
+            await message.delete()
+    elif message.content.startswith("$clear"):
+        if producers in message.author.roles:
+           # print("can delete")
+            num_clear=message.content
+            num_clear=num_clear[7:]
+            num_clear=int(num_clear)
+            if num_clear>25:
+                num_clear=25
+            await message.channel.purge(limit=num_clear)
+            #print("deleting messages")
+            await message.channel.send(f"{message.author.nick} deleted the last {num_clear} messages", delete_after=30)
     elif (message.channel==event_channel):
         yes="⬆️"
         no="⬇️"
@@ -150,6 +188,7 @@ async def on_message(message):
     elif message.content.startswith("$"):
         await message.channel.send('Invalid Command')
         await message.delete()
+
 @client.event
 async def on_reaction_add(reaction, user):
     event_channel=client.get_channel(881550954527326228)
@@ -166,4 +205,5 @@ async def on_reaction_add(reaction, user):
             await event_response.send(f"{user.nick} cannot come to {reaction.message.content}")
         if str(reaction.emoji) == "↔": 
             await event_response.send(f"{user.nick} could potentially come to {reaction.message.content}")
+
 client.run(TOKEN)
