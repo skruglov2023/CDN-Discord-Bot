875885a36b97f2ed79ad834e6ff26d6f6d381a6b
Stephan <skruglov.2023@carmelhs.org>
2022-05-04 21:25:21 -0500

    Forgot 1 thing

--- CDNEventBot.py
+++ CDNEventBot.py
@@ -140,6 +140,13 @@ async def reload_cog_error(ctx, error):
         await ctx.send(error)
 
 
+# This context menu command only works on members
+@bot.tree.context_menu(name='Show Join Date')
+async def show_join_date(interaction: discord.Interaction, member: discord.Member):
+    # The format_dt function formats the date time into a human readable representation in the official bot
+    await interaction.response.send_message(f'{member} joined at {discord.utils.format_dt(member.joined_at)}', ephemeral=True)
+
+
 @bot.tree.context_menu(name="Let dictators/Franklin know")
 async def report_message(interaction: discord.Interaction, message: discord.Message):
     # We're sending this response message with ephemeral=True, so only the command executor can see it
